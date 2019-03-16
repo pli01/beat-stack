15f6f8e8757f36952c802bbea6cc5f8cf9498df9
pli01 <pli01@noreply.github.com>
2019-03-17 00:05:24 +0100

    reduce tests (test config and modules)

--- apps/tests/run-env-test.sh
+++ apps/tests/run-env-test.sh
@@ -27,36 +27,5 @@ if [ "$test_result" -gt 0 ] ;then
 	test_status=FAILED
 fi
 echo "# $script_name: $test_name $test_status $test_output"
-
-# start all service
-test_status=OK
-test_name="${APP_DOCKER_COMPOSE_RUN} ${service_name}: "'metricbeat test config'
-echo "# $script_name: $test_name"
-#( cd ${APP_PATH} && ${DC} -f ${APP_DOCKER_COMPOSE_RUN} up --no-recreate --no-deps --no-build 2>&1 )
-test_output=$( ( cd ${APP_PATH} && ${DC} -f ${APP_DOCKER_COMPOSE_RUN} run --rm --no-deps ${DC_USE_TTY} ${service_name} /bin/bash -c "metricbeat --strict.perms=false --path.config /metricbeat -v test config" ) )
-test_result=$?
-if [ "$test_result" -gt 0 ] ;then
-	echo "ERROR: $test_name $test_result $test_output"
-	ret=$test_result
-	test_status=FAILED
-	exit $ret
-fi
-echo "# $script_name: $test_name $test_status $test_output"
-
-# start all service
-test_status=OK
-test_name="${APP_DOCKER_COMPOSE_RUN} ${service_name}: "'metricbeat test modules'
-echo "# $script_name: $test_name"
-#( cd ${APP_PATH} && ${DC} -f ${APP_DOCKER_COMPOSE_RUN} up --no-recreate --no-deps --no-build 2>&1 )
-test_output=$( ( cd ${APP_PATH} && ${DC} -f ${APP_DOCKER_COMPOSE_RUN} run --rm --no-deps ${DC_USE_TTY} ${service_name} /bin/bash -c "metricbeat --strict.perms=false --path.config /metricbeat -v test modules" ) )
-test_result=$?
-if [ "$test_result" -gt 0 ] ;then
-	echo "ERROR: $test_name $test_result"
-	ret=$test_result
-	test_status=FAILED
-	exit $ret
-fi
-echo "# $script_name: $test_name $test_status"
-
 set -e
 exit $ret
